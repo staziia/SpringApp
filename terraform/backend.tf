63ef2668e34b612b7ac34415657605b59fffd0e0
staziia <golyukna13@gmail.com>
2026-03-23 06:59:08 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-spring-boot-app-marianf"
+    bucket         = "terraform-state-spring-boot-app-stazii"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
